a1276db036736b326bfe29022f857670af2e2c06
Claire Felt <cfeltgungatan@gmail.com>
2024-10-26 16:30:29 -0600

    Almost full page

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const btnElement = document.querySelector('#hamburgerBtn');
+const navElement = document.querySelector('#primaryNav');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+}); 
+
+//Put current year in footer
+const theYear = new Date();
+document.querySelector('#year').textContent = theYear.getFullYear()
+
